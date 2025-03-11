8cf80f23f6fd8c254226dc413c6d39bce93fb920
ethanhetu <ethanhetu@gmail.com>
2025-03-11 11:48:00 -0500

    Update agent_dashboard.py

--- agent_dashboard.py
+++ agent_dashboard.py
@@ -418,6 +418,7 @@ def display_player_section(title, player_df):
 def arbitration_page():
     st.title("Arbitration")
     st.subheader("Overall Standings - Arbitration (Arb Filings Per Client)")
+    st.write("Agents are ranked based on the number of times they file for arbitration per client. The agents who less frequently file for arbitration, who therefore more frequently come to agreements before needing arbitration, are ranked more highly.")
     
     # Load data to get CT and Agency info
     _, ranks_data, _ = load_data()
