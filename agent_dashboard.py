3c26988dcc3007be06df20e002151f0143e25c45
ethanhetu <ethanhetu@gmail.com>
2025-04-01 14:16:16 -0500

    Update agent_dashboard.py

--- agent_dashboard.py
+++ agent_dashboard.py
@@ -119,6 +119,7 @@ manual_photo_overrides = {
     "tyler parsons": "https://lscluster.hockeytech.com/download.php?client_code=ahl&file_path=media/6a151b7d2ede020ae087381a1d054598.png",
     "evan cormier": "https://a.espncdn.com/combiner/i?img=/i/headshots/nhl/players/full/4064851.png&w=350&h=254",
     "nelson nogier": "https://a.espncdn.com/combiner/i?img=/i/headshots/nhl/players/full/3150129.png&w=350&h=254",
+    "josh healey": "https://assets.leaguestat.com/ahl/240x240/6687.jpg",
     "reid duke": "https://a.espncdn.com/combiner/i?img=/i/headshots/nhl/players/full/3150433.png"
 }
 
